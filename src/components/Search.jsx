55ff39f16d15d1a6ddc135157847dfeedce04c1b
miller0803 <millerluis0803@gmail.com>
2024-08-12 21:36:44 -0300

    to-do-list

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+const Search = ({ search, setSearch }) => {
+    return ( 
+    <div className="search">
+        <h2>Pesquisar:</h2>
+        <input 
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Digite sua pesquisa..." 
+        />
+    </div>
+    );       
+};
+
+export default Search;
